c6b33b1c46afbb72a0855654914fb418c34cbc5e
mal4ishka <nesterovvlad@gmail.com>
2024-08-20 19:49:51 +0300

    nesterov_commit
    додавання/видалення номерних знаків
    редагування даних клієнта
    бан номерних знаків

--- ParkEasy/admin_panel/urls.py
+++ ParkEasy/admin_panel/urls.py
@@ -6,4 +6,5 @@ app_name = 'admin_panel'
 urlpatterns = [
     path('', views.home, name='home'),
     path('users/', views.users, name='users'),
+    path('users/edit/<int:user_id>/', views.edit_user, name='edit_user'),
 ]
--- ParkEasy/parkings/models.py
+++ ParkEasy/parkings/models.py
@@ -11,3 +11,11 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"Username: {self.user}, Phone: {self.phone_number}, Email: {self.email}"
 
+
+class Plates(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='plates')
+    plate_number = models.CharField(max_length=20)
+    is_banned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.plate_number

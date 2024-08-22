c0ea8a57e3d00c00e56530064d4561d454a7fe90
Artur <arturlevuckuy@gmail.com>
2024-08-22 12:35:44 +0200

    profile

--- ParkEasy/users/models.py
+++ ParkEasy/users/models.py
@@ -5,7 +5,6 @@ class UserCreate(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     license_plate = models.CharField(max_length=10, unique=True)
     license_plate_image = models.ImageField(upload_to='license_plates/')
-    car_model = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username} - {self.license_plate}'
